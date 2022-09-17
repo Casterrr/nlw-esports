fb21d60d4a34fdf6e61dc39e823ecd53be3e42bb
Lucas <lucasmvl2003@gmail.com>
2022-09-17 12:18:59 -0300

    Stage 5: Final Round
    Finished all the application made during NLW

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from 'react'
 import { StatusBar } from "react-native";
+
 import {
   useFonts,
   Inter_400Regular,
@@ -11,13 +13,51 @@ import { Routes } from './src/routes';
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
 
+import { Subscription } from 'expo-modules-core'
+
+
+import "./src/services/notificationConfig"
+import * as Notifications from 'expo-notifications'
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
+
+
 export default function App() {
+  const getNotificationListener = useRef<Subscription>()
+	const responseNotificationListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, [])
+
+  useEffect(() => {
+		getNotificationListener.current =
+			Notifications.addNotificationResponseReceivedListener((notification) => {
+				console.log(notification)
+			})
+
+		responseNotificationListener.current =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				console.log(response)
+			})
+
+		return () => {
+			if (getNotificationListener.current && responseNotificationListener.current) {
+				Notifications.removeNotificationSubscription(getNotificationListener.current)
+				Notifications.removeNotificationSubscription(
+					responseNotificationListener.current
+				)
+			}
+		}
+	}, [])
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_900Black,
   });
+
 
   return (
     <Background>
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -17,9 +17,11 @@ import { styles } from './styles';
 import { GameParams } from "../../@types/navigation";
 import { Heading } from "../../components/Heading";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { DuoMatch } from "../../components/DuoMatch";
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -28,6 +30,12 @@ export function Game() {
   function handleGoBack() {
     navigation.goBack();
   }
+
+  async function getDiscordUser(adsId: string) {
+		fetch(`http://192.168.0.2:3333/ads/${adsId}/discord`)
+			.then((response) => response.json())
+			.then((data) => setDiscordDuoSelected(data.discord))
+	}
 
   useEffect(() => {
     fetch(`http://192.168.0.2:3333/games/${game.id}/ads`)
@@ -74,7 +82,7 @@ export function Game() {
             renderItem={({ item }) => (
               <DuoCard
                 data={item}
-                onConnect={() => {}}
+                onConnect={() => getDiscordUser(item.id)}
               />
             )}
             horizontal
@@ -86,6 +94,12 @@ export function Game() {
                 Não há anúncios publicados ainda.
               </Text>
             }
+          />
+
+          <DuoMatch 
+            visible={discordDuoSelected.length > 0} 
+            discord={discordDuoSelected}
+            onClose={() => setDiscordDuoSelected('')}
           />
 
         
